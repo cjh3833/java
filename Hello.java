b990e77de5a8fa9600df9556e3fb905ed23f065c
cjh3833 <39274352+cjh3833@users.noreply.github.com>
2024-01-10 20:48:46 +0900

    2024_01_10_2

--- Hello.java
+++ Hello.java
@@ -1,13 +1,38 @@
+
 /*
         Date : 2024.01.08
         Source : Hello java
         Author : 최재형
 */
+import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Test");
-        System.out.println()
+        char ch = 'A';
+        int code = (int) ch;
+        System.out.printf("%c = %d(%#X) %n", ch, code, code);
+
+        char hch = '가';
+        System.out.printf("%c = %d(%#X)%n", hch, (int) hch, (int) hch);
+
+        /*
+         * Scanner scanner = new Scanner(System.in); // Scanner 클래스의 객체를 생성
+         * String input = scanner.nextLine(); // 입력받은 내용을 input에 저장
+         * int num = Integer.parseInt(input); // 입력받은 내용을 int 타입의 값으로 변환
+         * 
+         * System.out.println("입력내용 : " + input);
+         * System.out.printf("num = %d\n", num);
+         */
+
+        /*
+         * int finger = 10;
+         * char c = 'A';
+         * float f3 = 3.14e3f;
+         * System.out.printf("finger = [%5d]\n", finger);
+         * System.out.printf("finger = [%-5d]\n", finger);
+         * System.out.printf("finger = [%010d]\n", finger);
+         * System.out.printf("c = %c, %d\n", c, (int) c);
+         * System.out.printf("%f, %e, %g", f3, f3, f3);
+         */
     }
 }
