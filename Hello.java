584f5e0786ba5db6a62516b977f36bd0107ba36c
cjh3833 <39274352+cjh3833@users.noreply.github.com>
2024-01-10 16:58:11 +0900

    2024_01_10

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
+/*
+        Date : 2024.01.08
+        Source : Hello java
+        Author : 최재형
+*/
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Test");
+        System.out.println()
     }
 }
