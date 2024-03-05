a70c091bfbb50b9493e4c6cb20d1bde0f417b813
cjh3833 <39274352+cjh3833@users.noreply.github.com>
2024-03-05 15:15:28 +0900

    예외처리

--- Java_exception_handling.java
+++ Java_exception_handling.java
@@ -1,33 +1,22 @@
 import java.io.*;
 
-class ExceptionEx15 {
+class ExceptionEx16 {
     public static void main(String[] args) {
-        // command line에서 입력받은 값을 이름으로 가지는 파일을 생성한다
-        File f = createFile(args[0]);
-        System.out.println(f.getName() + "파일이 성공적으로 생성되었습니다");
+        try {
+            File f = createFile(args[0]);
+            System.out.println(f.getName() + "파일이 성공적으로 생성되었습니다");
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + "다시 입력해주세요");
+        }
     }
 
-    static File createFile(String fileName) {
-        try {
-            if (fileName == null || fileName.equals(""))
-                ;
+    static File createFile(String fileName) throws Exception {
+        if (fileName == null || fileName.equals(""))
             throw new Exception("파일 이름이 유효하지 않습니다");
-        } catch (Exception e) {
-            fileName = "제목없음.txt";
-        } finally {
-            File f = new File(fileName);
-            createNewFile(f);
-            return f;
-        }
+        File f = new File(fileName);
+        f.createNewFile();
+        return f;
     }
-
-    static void createNewFile(File f) {
-        try {
-            f.createNewFile();
-        } catch (Exception e) {
-        }
-    }
-
 }
 
 /*
