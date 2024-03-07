8a9248240a6bacb1b7ec2ada2ccc6f8d8e334d3b
cjh3833 <39274352+cjh3833@users.noreply.github.com>
2024-03-07 15:43:59 +0900

    예외 끝

--- Java_Object2.java
+++ Java_Object2.java
@@ -1,19 +1,164 @@
 import java.awt.*;
 import java.awt.event.*;
 
-class InnerEx7 {
-    public static void mai(String[] args) {
-        Button b = new Button("Start");
-        b.addActionListener(new EventHandler());
+class ChainedExceptionEx {
+    public static void main(String[] args) {
+        try {
+            install();
+        } catch (InstallException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    static void install() throws InstallException {
+        try {
+            startInstall();
+            copyFiles();
+        } catch (SpaceException se) {
+            InstallException ie = new InstallException("설치중 예외 발생");
+            ie.initCause(se);
+        } catch (MemoryException me) {
+            InstallException ie = new InstallException("설치 중 예외발생");
+            ie.initCause(me);
+            throw ie;
+        } finally {
+            deleteTempFiles();
+        }
+    }
+
+    static void startInstall() throws SpaceException, MemoryException {
+        if (!enoughSpace()) {
+            throw new SpaceException("설치할 공간이 부족합니다");
+        }
+
+        if (!enoughMemory()) {
+            throw new MemoryException("메모리가 부족합니다");
+            // throw new RuntimeException(new MemoryException("메모리가 부족합니다"));
+        }
+    }
+
+    static void copyFiles() {
+    }
+
+    static void deleteTempFiles() {
+    }
+
+    static boolean enoughSpace() {
+        // 설치에 필요한 공간있는지 확인하는 코드
+        return false;
+    }
+
+    static boolean enoughMemory() {
+        // 설치하는데 필요한 메모리 공간이 있는지 확인하는 코드
+        return true;
     }
 }
 
-class EventHandler implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("ActionEvent occurred!!!");
+class InstallException extends Exception {
+    InstallException(String msg) {
+        super(msg);
     }
 }
+
+class SpaceException extends Exception {
+    SpaceException(String msg) {
+        super(msg);
+    }
+}
+
+class MemoryException extends Exception {
+    MemoryException(String msg) {
+        super(msg);
+    }
+}
+
+/*
+ * class Exception17 {
+ * public static void main(String[] args) {
+ * try {
+ * method1();
+ * } catch (Exception e) {
+ * System.out.println("main메서드에서 예외가 처리되었습니다");
+ * }
+ * }
+ * 
+ * static void method1() throws Exception {
+ * try {
+ * throw new Exception();
+ * } catch (Exception e) {
+ * System.out.println("method1메서드에서 예외가 처리되었습니다");
+ * throw e;
+ * }
+ * }
+ * }
+ */
+
+/*
+ * class TryWithResourceEx { // 자바 버전 1.7부터 사용가능한 try-with-resource문
+ * public static void main(String args[]) {
+ * try (CloseableResource cr = new CloseableResource()) {
+ * cr.exceptionWork(false); // 예외가 발생하지않는다
+ * } catch (WorkException e) {
+ * e.printStackTrace();
+ * } catch (CloseException e) {
+ * e.printStackTrace();
+ * }
+ * System.out.println();
+ * 
+ * try (CloseableResource cr = new CloseableResource()) {
+ * cr.exceptionWork(true);
+ * } catch (WorkException e) {
+ * e.printStackTrace();
+ * } catch (CloseException e) {
+ * e.printStackTrace();
+ * }
+ * }
+ * }
+ * 
+ * class CloseableResource implements AutoCloseable {
+ * public void exceptionWork(boolean exception) throws WorkException {
+ * System.out.println("exceptionWork(" + exception + ") 가 호출됨");
+ * 
+ * if (exception)
+ * throw new WorkException("WorkException발생");
+ * }
+ * 
+ * public void close() throws CloseException {
+ * System.out.println("close()가 호출됨");
+ * throw new CloseException("CloseException발생");
+ * }
+ * }
+ * 
+ * class WorkException extends Exception {
+ * WorkException(String msg) {
+ * super(msg);
+ * }
+ * }
+ * 
+ * class CloseException extends Exception {
+ * CloseException(String msg) {
+ * super(msg);
+ * }
+ * }
+ */
+
+/*
+ * class InnerEx7 {
+ * public static void mai(String[] args) {
+ * Button b = new Button("Start");
+ * b.addActionListener(new EventHandler());
+ * 
+ * }
+ * }
+ * 
+ * class EventHandler implements ActionListener {
+ * public void actionPerformed(ActionEvent e) {
+ * System.out.println("ActionEvent occurred!!!");
+ * }
+ * }
+ */
 
 /*
  * class InnerEx6 {
